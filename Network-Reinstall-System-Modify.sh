03a4136af0b991653cbab9793255915a85f2087b
MeowLove <MeowLove@users.noreply.github.com>
2019-01-06 16:33:57 +0800

    Version 2.0.1
    [Version 2.0.1]
    The latest stable version.
    
    Happy New Year! Thanks for your waiting!
    Starting with V2.0.1, you are allowed to install any system from a network remote ISO or iPXE.

---
+++ Network-Reinstall-System-Modify.sh
@@ -0,0 +1,169 @@
+#!/bin/bash
+
+## This is the magically modified version of the one-click reload script.
+## It can reinstall CentOS, Debian, Ubuntu and other Linux systems (continuously added) over the network in one click.
+## It can reinstall Windwos 2003, 7, 2008R2, 2012R2, 2016, 2019 and other Windows systems (continuously added) via the network in one click.
+## Technical support is provided by the technical blog. (based on the original version of Vicer)
+
+## Magic Modify version author:
+## Support GRUB or GRUB2 for installing a clean minimal system.
+## Default root password: cxthhhhh.com
+## Blog: https://tech.cxthhhhh.com
+## Written By Technical Blog
+
+## Original version author:
+## Suitable for using by GRUB.
+## Blog: https://moeclub.org
+## Written By Vicer
+
+
+echo -e "\n\n\n"
+clear
+echo -e "\n"
+echo "---------------------------------------------------------------------------------------------------------------------"
+echo -e "\033[33m Network-Reinstall-System-Modify Tools V2.0.1 2019/01/06 \033[0m"
+echo -e "\033[33m [Magic Modify] Reinstall the system (any Windows / Linux) requires only network and one click \033[0m"
+echo -e "\033[33m System requirements: Any Linux system with GRUB or GRUB2, recommended CentOS7/Debian9/Ubuntu18.04 \033[0m"
+echo -e "\n"
+echo -e "\033[33m [Original] One-click Network Reinstall System - Magic Modify version (For Linux/Windows) \033[0m"
+echo -e "\033[33m https://tech.cxthhhhh.com/linux/2018/11/27/original-one-click-network-reinstall-system-magic-modify-version-for-linux-windows-en.html \033[0m"
+echo "---------------------------------------------------------------------------------------------------------------------"
+echo -e "\n"
+sleep 5s
+
+
+if [ $1 = '-CentOS_7' ]
+then
+	echo -e "\033[33m You have chosen to install the latest CentOS_7 \033[0m"
+	echo -e "\n"
+	sleep 2s
+	wget --no-check-certificate -qO koiok.sh 'https://tech.cxthhhhh.com/tech-tools/Network-Reinstall-System-Modify/koiok.sh' && bash koiok.sh -dd 'https://opendisk.cxthhhhh.com/OperatingSystem/CentOS/CentOS_7.X_NetInstallation.vhd.gz'
+fi
+
+if [ $1 = '-CentOS_6' ]
+then
+	echo -e "\033[33m You have chosen to install the latest CentOS_6 \033[0m"
+	echo -e "\n"
+	sleep 2s
+	wget --no-check-certificate -qO koiok.sh 'https://tech.cxthhhhh.com/tech-tools/Network-Reinstall-System-Modify/koiok.sh' && bash koiok.sh -c 6.9 -v 64 -a
+fi
+
+if [ $1 = '-Debian_9' ]
+then
+	echo -e "\033[33m You have chosen to install the latest Debian_9 \033[0m"
+	echo -e "\n"
+	sleep 2s
+	wget --no-check-certificate -qO koiok.sh 'https://tech.cxthhhhh.com/tech-tools/Network-Reinstall-System-Modify/koiok.sh' && bash koiok.sh -d 9 -v 64 -a
+fi
+
+if [ $1 = '-Debian_8' ]
+then
+	echo -e "\033[33m You have chosen to install the latest Debian_8 \033[0m"
+	echo -e "\n"
+	sleep 2s
+	wget --no-check-certificate -qO koiok.sh 'https://tech.cxthhhhh.com/tech-tools/Network-Reinstall-System-Modify/koiok.sh' && bash koiok.sh -d 8 -v 64 -a
+fi
+
+if [ $1 = '-Debian_7' ]
+then
+	echo -e "\033[33m You have chosen to install the latest Debian_7 \033[0m"
+	echo -e "\n"
+	sleep 2s
+	wget --no-check-certificate -qO koiok.sh 'https://tech.cxthhhhh.com/tech-tools/Network-Reinstall-System-Modify/koiok.sh' && bash koiok.sh -d 7 -v 64 -a
+fi
+
+if [ $1 = '-Ubuntu_18.04' ]
+then
+	echo -e "\033[33m You have chosen to install the latest Ubuntu_18.04 \033[0m"
+	echo -e "\n"
+	sleep 2s
+	wget --no-check-certificate -qO koiok.sh 'https://tech.cxthhhhh.com/tech-tools/Network-Reinstall-System-Modify/koiok.sh' && bash koiok.sh -u 18.04 -v 64 -a
+fi
+
+if [ $1 = '-Ubuntu_16.04' ]
+then
+	echo -e "\033[33m You have chosen to install the latest Ubuntu_16.04 \033[0m"
+	echo -e "\n"
+	sleep 2s
+	wget --no-check-certificate -qO koiok.sh 'https://tech.cxthhhhh.com/tech-tools/Network-Reinstall-System-Modify/koiok.sh' && bash koiok.sh -u 16.04 -v 64 -a
+fi
+
+if [ $1 = '-Ubuntu_14.04' ]
+then
+	echo -e "\033[33m You have chosen to install the latest Ubuntu_14.04 \033[0m"
+	echo -e "\n"
+	sleep 2s
+	wget --no-check-certificate -qO koiok.sh 'https://tech.cxthhhhh.com/tech-tools/Network-Reinstall-System-Modify/koiok.sh' && bash koiok.sh -u 14.04 -v 64 -a
+fi
+
+if [ $1 = '-Windows_Server_2019' ]
+then
+	echo -e "\033[33m You have chosen to install the latest Windows_Server_2019 \033[0m"
+	echo -e "\n"
+	sleep 2s
+	wget --no-check-certificate -qO koiok.sh 'https://tech.cxthhhhh.com/tech-tools/Network-Reinstall-System-Modify/koiok.sh' && bash koiok.sh -dd 'https://opendisk.cxthhhhh.com/OperatingSystem/Windows/Disk_Windows_DD/Disk_Windows_Server_2019_DataCenter_CN.vhd.gz'
+fi
+
+if [ $1 = '-Windows_Server_2016' ]
+then
+	echo -e "\033[33m You have chosen to install the latest Windows_Server_2016 \033[0m"
+	echo -e "\n"
+	sleep 2s
+	wget --no-check-certificate -qO koiok.sh 'https://tech.cxthhhhh.com/tech-tools/Network-Reinstall-System-Modify/koiok.sh' && bash koiok.sh -dd 'https://opendisk.cxthhhhh.com/OperatingSystem/Windows/Disk_Windows_DD/Disk_Windows_Server_2016_DataCenter_CN.vhd.gz'
+fi
+
+if [ $1 = '-Windows_Server_2012R2' ]
+then
+	echo -e "\033[33m You have chosen to install the latest Windows_Server_2012R2 \033[0m"
+	echo -e "\n"
+	sleep 2s
+	wget --no-check-certificate -qO koiok.sh 'https://tech.cxthhhhh.com/tech-tools/Network-Reinstall-System-Modify/koiok.sh' && bash koiok.sh -dd 'https://opendisk.cxthhhhh.com/OperatingSystem/Windows/Disk_Windows_DD/Disk_Windows_Server_2012R2_DataCenter_CN.vhd.gz'
+fi
+
+if [ $1 = '-Windows_Server_2008R2' ]
+then
+	echo -e "\033[33m You have chosen to install the latest Windows_Server_2008R2 \033[0m"
+	echo -e "\n"
+	sleep 2s
+	wget --no-check-certificate -qO koiok.sh 'https://tech.cxthhhhh.com/tech-tools/Network-Reinstall-System-Modify/koiok.sh' && bash koiok.sh -dd 'https://opendisk.cxthhhhh.com/OperatingSystem/Windows/Disk_Windows_DD/Disk_Windows_Server_2008R2_DataCenter_CN.vhd.gz'
+fi
+
+if [ $1 = '-Windows_7_Vienna' ]
+then
+	echo -e "\033[33m You have chosen to install the latest Windows_7_Vienna \033[0m"
+	echo -e "\n"
+	sleep 2s
+	wget --no-check-certificate -qO koiok.sh 'https://tech.cxthhhhh.com/tech-tools/Network-Reinstall-System-Modify/koiok.sh' && bash koiok.sh -dd 'https://opendisk.cxthhhhh.com/OperatingSystem/Windows/Disk_Windows_DD/Disk_Windows_7_Vienna_Ultimate_CN.vhd.gz'
+fi
+
+if [ $1 = '-Windows_Server_2003' ]
+then
+	echo -e "\033[33m You have chosen to install the latest Windows_Server_2003 \033[0m"
+	echo -e "\n"
+	sleep 2s
+	wget --no-check-certificate -qO koiok.sh 'https://tech.cxthhhhh.com/tech-tools/Network-Reinstall-System-Modify/koiok.sh' && bash koiok.sh -dd 'https://opendisk.cxthhhhh.com/OperatingSystem/Windows/Disk_Windows_DD/Disk_Windows_Server_2003_DataCenter_CN.vhd.gz'
+fi
+
+if [ $1 = '-CXT_Bare-metal_System_Deployment_Platform' ]
+then
+	echo -e "\033[33m You have chosen to install the latest CXT_Bare-metal_System_Deployment_Platform \033[0m"
+	echo -e "\n"
+	sleep 2s
+	wget --no-check-certificate -qO koiok.sh 'https://tech.cxthhhhh.com/tech-tools/Network-Reinstall-System-Modify/koiok.sh' && bash koiok.sh -dd 'https://opendisk.cxthhhhh.com/OperatingSystem/CXT-System/CXT_Bare-metal_System_Deployment_Platform.vhd.gz'
+fi
+
+if [ $1 = '-DD' ]
+then
+	echo -e "\033[33m You have chosen to install the DD package provided by you \033[0m"
+	echo -e "\n"
+	sleep 2s
+	wget --no-check-certificate -qO koiok.sh 'https://tech.cxthhhhh.com/tech-tools/Network-Reinstall-System-Modify/koiok.sh' && bash koiok.sh -dd $2
+fi
+
+
+echo "---------------------------------------------------------------------------------------------------------------------"
+echo -e "\033[31m !!!!!!!!Error!!!!!!!!Error!!!!!!!!Error!!!!!!!! \033[0m"
+echo -e "\033[31m Please check if the options are correct. For example [-CXT_Bare-metal_System_Deployment_Platform/-CentOS_7/-Windows_Server_2019]/-DD '%URL' \033[0m"
+echo "---------------------------------------------------------------------------------------------------------------------"
+echo -e "\n"
+exit
--- README.md
+++ README.md
@@ -1,2 +1,55 @@
 # Network-Reinstall-System-Modify
-It can reinstall CentOS, Debian, Ubuntu, Windwos 2003, 7, 2008R2, 2012R2, 2016, 2019 and other systems (continuously added) via the Internet.
+It can reinstall CentOS, Debian, Ubuntu, Windows 2003, 7, 2008R2, 2012R2, 2016, 2019 and other systems (continuously added) via the Internet, and can install any system via mounting remote network ISO.
+
+[One-click Network Reinstall System – Magic Modify version](https://tech.cxthhhhh.com/linux/2018/11/27/original-one-click-network-reinstall-system-magic-modify-version-for-linux-windows-en.html), Forked from [MoeClub Vicer](https://moeclub.org/2018/04/03/603/), technical support and maintenance provided by [Technical Blog | 技術博客](https://tech.cxthhhhh.com/), more features of the magic version are constantly increasing.
+
+
+## Realistic Demand
+Why do we need to reinstall a pure system?
+
+1. The system template provided by the service provider may have some built-in software, even conflicting with the software we are about to install, resulting in the installation failure.
+
+2. ISO mount is not a service provided by all service providers. Some IPIM/KVM transmission speeds are too slow and installation efficiency is poor.
+
+3. Linux/Windows may encounter some inexplicable errors that cannot be found in use. I believe you must have a deep understanding!
+
+
+## You Need To Understand
+Where are the release notes and tutorials?
+
+1. If you are from an English community, please click here.
+https://tech.cxthhhhh.com/linux/2018/11/27/original-one-click-network-reinstall-system-magic-modify-version-for-linux-windows-en.html
+
+2. 如果你来自于中文社区，请点击这里
+https://tech.cxthhhhh.com/linux/2018/11/29/original-one-click-network-reinstall-system-magic-modify-version-for-linux-windows-cn.html
+
+
+## Common Problem
+Here and the tutorials in my blog will cover the answers to most questions.
+
+1. Q：The default password for the system installation?
+A：The default password for all system installations is [cxthhhhh.com]. To prevent brute force cracking, you must change the default password immediately after installation!
+
+2. Q：How to connect and control my new system？
+A：Due to different hardware configuration and network environment, the installation takes 15 to 60 minutes, please be patient. Once the installation is complete, you can connect via IP: 22 (Linux SSH) / IP: 3389 (Windows RDP).
+
+3. Q：How to manually install any system using [Bare-metal_System_Deployment_Platform]?
+A：It's hard to describe the process in one sentence, so visit my blog and follow the tutorial.
+
+4. Q：Why isn't the content of the project stored on Github not all?
+A：As we all know, Github can only upload files smaller than 100MB. However, as of January 1, 2019, all files in the entire Network-Reinstall-System-Modify project totaled more than 50GB. So I can only upload script content and smaller files, including various image files and the auxiliary platform I built will not be able to upload. But you can visit [Open Disk CDN](https://opendisk.cxthhhhh.com/) to learn more.
+
+5. Q：How to communicate? Feedback question?[Support language English (Recommended), Chinese (中文).]
+A：You can join my Telegram channel and discussion groups for instant communication and feedback. You can also submit an issue with Issues, but this is not immediate, but it is easy to track.
+[Telegram Channel：My Share](https://t.me/me_share)
+[Telegram Chat Group：Technical Blog | 技術博客](https://t.me/Technical_Blog)
+
+
+## End Notes
+[Network-Reinstall-System-Modify](https://tech.cxthhhhh.com/linux/2018/11/27/original-one-click-network-reinstall-system-magic-modify-version-for-linux-windows-en.html)
+
+Version：V2.0.1 
+Date：2019/01/06
+
+[Technical Blog | 技術博客](https://tech.cxthhhhh.com/)
+https://tech.cxthhhhh.com/
